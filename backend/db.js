72d3246ef875c327c1bb4b6c80847b8d6e0fbc89
abanishtr <abanisht558@gmail.com>
2023-06-14 15:10:33 +0545

    basic setup

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const mongoURI = "mongodb+srv://abanisht558:atrleo@cluster0.uz3jozg.mongodb.net/
 async function connectToMongo() {
     try {
       await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-      console.log('Connected to Database');
+      console.log('Connected to database sucessfully ');
   
     } catch (error) {
       console.error('Error connecting to the database', error);
--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,10 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('This is abanish tripathi sending request')
-})
+
+// available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
