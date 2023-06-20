ab43892a53d13aab776351384fbf62b7255acfef
abanishtr <abanisht558@gmail.com>
2023-06-20 14:07:58 +0545

    hashing password

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://abanisht558:atrleo@cluster0.uz3jozg.mongodb.net/"
+const mongoURI = "mongodb+srv://abanisht558:atrleo@cluster0.uz3jozg.mongodb.net/enotebook"
 
 
 async function connectToMongo() {
     try {
       await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-      console.log('Connected to database sucessfully ');
+      console.log('Connected to database sucessfully  ');
   
     } catch (error) {
       console.error('Error connecting to the database', error);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
+const JWT_SECRET = "signthewebtoken"; 
 
 
 // Create a User using: POST "/api/auth/createuser".No login required
@@ -27,15 +30,26 @@ router.post('/createuser', [
       return res.status(400).json({ error: "The user with this email already exist." })
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const securePass = await bcrypt.hash(req.body.password, salt);
+
+    // create a new user
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
-    })
+      password: securePass,
+    });
     //   .then(user => res.json(user))
     //   .catch(err=>{console.log(err)
     // res.json({error: 'please enter unique value for email',message : err.message})})
-    res.json(user);
+   const data= {
+    user : {
+      id: user.id
+    }
+   }
+  const authtoken = jwt.sign(data,JWT_SECRET);
+  
+    res.json({authtoken});
   }
   catch (error) {
     console.error(error.message);
