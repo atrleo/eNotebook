9da0b2ceb2192b955172f45254b9c227c506d49b
abanishtr <abanisht558@gmail.com>
2023-06-26 14:01:48 +0545

    Added feature to update notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -24,7 +24,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 
 
 // Route 2:  Add a new notes: POST "/api/notes/addnote". :login required
-router.get('/addnote', fetchuser, [
+router.post('/addnote', fetchuser, [
     body('title', 'Enter a valid Title').isLength({ min: 3 }),
     body('description', 'description must be atleast 5 character').isLength({ min: 5 }),
 
@@ -55,4 +55,37 @@ router.get('/addnote', fetchuser, [
     }
 
 })
+
+
+// Route 3:  Updating the existing notes:  POST "/api/notes/updatenote". : login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+    try {
+      const {title, description , tag} = req.body;
+
+    //  creating a new note object
+      const newNote = {};
+
+      if(title){newNote.title = title};
+      if(description){newNote.description = description};
+      if(tag){newNote.tag = tag};
+
+    //   find the note to be updated and update it
+    let note = await Notes.findById(req.params.id);
+        if(!note){return res.status(404).send("Not found")};
+
+        if(note.user.toString() !== req.user.id){return res.status(401).send("Not allowed")};
+
+        note = await Notes.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true});
+        res.json({note});
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+
+    }
+
+
+})
 module.exports = router; 
+
