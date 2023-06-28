297b4d6372680303ecfab6c47e6aa8425c508e40
abanishtr <abanisht558@gmail.com>
2023-06-28 18:41:43 +0545

    Creatinga navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as 
+  Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1 style={{ alignItems: 'center', textAlign: 'center', color: 'darkgrey' }}>This is eNotebook</h1>
+
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/home" element={<Home />}></Route>
+            <Route exact path="/about" element={<About />}></Route>
+          </Routes>
+        </Router>
+      </div>
+
+
+
+
+    </>
   );
 }
 
